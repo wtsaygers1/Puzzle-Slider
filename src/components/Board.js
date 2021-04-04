242e8b78fb5943ac0db067c570a4248f72bdbdc2
wtsaygers1 <wtsaygers1@gmail.com>
2021-04-04 19:21:04 -0400

    tiles clickable

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Tile from "./Tile";
 // import './Board.css';
 
+// tiles to click and console.log that the click is working
+// tiles to swap with no logic
+// tiles to swap with game logic
+
 class Board extends React.Component {
   constructor(props) {
     super(props);
@@ -10,7 +14,7 @@ class Board extends React.Component {
       won: false,
       N: 4,
     }
-
+    this.tileClick.bind(this);
   }
   componentDidMount() {
     let tempArr = [];
@@ -24,16 +28,19 @@ class Board extends React.Component {
         }
       )
     }
-
     this.setState({ Board: tempArr })
+  }
+
+  tileClick() {
+    console.log("cleeked")
   }
 
   render() {
     return (
       <div className="container">
         <div className="row">
-          {this.state.Board.map((tile, index) => 
-            <Tile key={index} id={tile.id} currentPos={tile.currentPos} winPos={tile.winPos}/>)}
+          {this.state.Board.map((tile, index) =>
+            <Tile key={index} id={tile.id} currentPos={tile.currentPos} winPos={tile.winPos} tileClick={this.tileClick} />)}
         </div>
       </div>
     )
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Tile(props){
     return(
-        <div className= "col-3 border border-dark">
+        <div className= "col-3 border border-dark" onClick={() => (props.tileClick())}>
             <p>id:{props.id}</p>
             <p>currentPos:{props.currentPos}</p>
             <p>winPos:{props.winPos}</p>
